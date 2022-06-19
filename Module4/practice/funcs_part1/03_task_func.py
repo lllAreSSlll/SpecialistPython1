bcd2f522eccbc0a384d22f02834a63465d85fd49
lllAreSSlll <106911811+lllAreSSlll@users.noreply.github.com>
2022-06-19 16:39:12 +0300

    Update 03_task_func.py

--- Module4/practice/funcs_part1/03_task_func.py
+++ Module4/practice/funcs_part1/03_task_func.py
@@ -1,12 +1,8 @@
-# Даны координаты двух точек на плоскости.
-# Напишите функцию нахождения расстояния между двумя точками.
-
 def distance(x1, y1, x2, y2):
-    # TODO: your code here
-    pass
-
+    dist = ((x2-x1)**2+(y2-y1)**2)**0.5
+    return dist
 
 # Тестируем функцию
-print(distance(2, 4, 2, 9))
-print(distance(12, 8, 12, -9))
-print(distance(23, 0, 15, 32))
+print(round(distance(2, 4, 2, 9),2))
+print(round(distance(12, 8, 12, -9),2))
+print(round(distance(23, 0, 15, 32),2))
